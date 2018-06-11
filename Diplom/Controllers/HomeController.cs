b829f0848756b85627e6bfc0d02e5ebcd8b26fc7
Michael Tronin <darksmile125@gmail.com>
2018-06-12 04:31:36 +0500

    вывод таблицы из бд

--- Diplom/Controllers/HomeController.cs
+++ Diplom/Controllers/HomeController.cs
@@ -1,30 +1,37 @@
 ﻿using Diplom.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace Diplom.Controllers
 {
     public class HomeController : Controller
     {
-       
+        ProfileContext db = new ProfileContext();
 
         public ActionResult Index()
-        { 
+        {
+            
+
             return View();
         }
 
-        ProfilesContext db = new ProfilesContext();
+        public ActionResult Profiles()
+        {
+            
+
+            return View(db.Profiles.ToList());
+        }
+        
+        
 
         public ActionResult About()
         {
-            //получаем из бд все объекты Profiles
-            IEnumerable<Profiles> profiles = db.Profiles;
-            //передаем все объекты в динамическое свойство Profiles в ViewBag
-            ViewBag.Profile = profiles;
-            //возвращаем представление
+            
             return View();
         }
 
--- Diplom/Models/Profiles.cs
+++ Diplom/Models/Profiles.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
+
 namespace Diplom.Models
 {
-    public class Profiles
+    public class Profiles 
     {
-        public int ProfileId { get; set; }
-
-        public string ProfileName { get; set;}
-
-        public string ProfileInfo { get; set; }
+        [Key]
+        public int ProfileID { get; set; }
+        
+        public string ProfileName { get; set; }
     }
 }
